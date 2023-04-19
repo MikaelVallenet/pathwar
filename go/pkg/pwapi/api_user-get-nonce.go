dfff288941efc268a9689b014f54bbfd124b310e
Mikatech <mikael.vallenet@epitech.eu>
2023-04-19 14:31:57 +0200

    feat(api): create skeleton of nonce & verify API route

---
+++ go/pkg/pwapi/api_user-get-nonce.go
@@ -0,0 +1,8 @@
+package pwapi
+
+import "context"
+
+func (svc *service) UserGetNonce(ctx context.Context, in *UserGetNonce_Input) (*UserGetNonce_Output, error) {
+	ret := &UserGetNonce_Output{}
+	return ret, nil
+}
---
+++ go/pkg/pwapi/api_user-signature.go
@@ -0,0 +1,8 @@
+package pwapi
+
+import "context"
+
+func (svc *service) UserSignature(ctx context.Context, in *UserSignature_Input) (*UserSignature_Output, error) {
+	ret := &UserSignature_Output{}
+	return ret, nil
+}
